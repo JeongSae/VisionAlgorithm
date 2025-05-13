71e83f7206f1ecb1544766c5cb89a428e0361947
JeongSae <not12m10@gmail.com>
2025-05-13 15:02:19 +0900

    Crop 작업, App 수정

---
+++ AlgorithmPython/ImageCrop.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+# cropped_image = ImageCrop.Crop(original_image, crop_area)
+def ImageCrop(original_image, crop_area):
+    cv_image = np.array(original_image)
+    
+    # Define the crop area
+    x, y, w, h = crop_area
+    
+    # Perform cropping
+    cropped_image = cv_image[y:y+h, x:x+w]
+    
+    # Convert back to PIL Image
+    cropped_image = Image.fromarray(cropped_image)
+    
+    return cropped_image
